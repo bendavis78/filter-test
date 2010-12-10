6407a2cc4ee23576f1709446897c8a1facd84493
Ben Davis <bendavis78@gmail.com>
2010-12-10 17:47:21 -0600

    added test db. username: admin, pass: admin

--- filtertest/models.py
+++ filtertest/models.py
@@ -1,12 +1,5 @@
 from django.db import models
 
-# Create your models here.
-class Food(models.Model):
-    name = models.CharField(max_length=100)
-    def __unicode__(self):
-        return self.name
-
-
 class Company(models.Model):
     name = models.CharField(max_length=100)
 
